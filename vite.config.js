86aba5a98bafe45f8aa2e73acf7781cc778db9d3
AlanaDubie <alanadubie922@gmail.com>
2026-04-18 14:55:17 -0700

    fixed image routing in vite config, updated tropical plants images

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()], 
-  base: '/portfolio/',
+export default defineConfig(({ command }) => ({
+  plugins: [react(), tailwindcss()],
+  base: command === 'build' ? '/portfolio/' : '/',
   resolve: {
     alias: {
       '@': '/src',
     },
   },
-})
+}))
