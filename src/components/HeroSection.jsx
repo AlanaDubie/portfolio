e1b0ccf8155925a6f833920ae869ff6bb07bed86
AlanaDubie <alanadubie922@gmail.com>
2026-04-18 14:43:15 -0700

    Image pathing on home page, moved photos to /public

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ export const HeroSection = () => {
     >
         <video autoPlay muted loop playsInline 
             className="w-full xl:h-screen object-cover">
-            <source src="src/assets/HeroVideo.mp4" type="video/mp4" />
+            <source src="/assets/HeroVideo.mp4" type="video/mp4" />
         </video>
         <a href="#about" className="absolute bottom-8 left-1/2 -translate-x-1/2 text-white/70 hover:text-white transition-colors">
             <ChevronDown size={36} />
